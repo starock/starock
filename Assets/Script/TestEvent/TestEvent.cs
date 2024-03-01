6a95a25173b727561dee548451b3f170e8a0b397
Stormer <s2010@126.com>
2024-03-02 01:07:36 +0800

    测试事件系统

--- Assets/Script/TestEvent/TestEvent.cs
+++ Assets/Script/TestEvent/TestEvent.cs
@@ -13,12 +13,20 @@ public struct ParamTest
 
 
 public class TestEvent : MonoBehaviour
-{
-    private void Awake()
+{ 
+    private void OnEnable()
     {
+        Debug.Log("Register events");
         EventManager.Register(EventIDs.Test1, new Action<ParamTest>(Test));
         EventManager.Register(EventIDs.Test2, new Action<int>(Test2));
-    } 
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Unregister events");
+        EventManager.Unregister(EventIDs.Test1, new Action<ParamTest>(Test));
+        EventManager.Unregister(EventIDs.Test2, new Action<int>(Test2));
+    }
 
     private void Test(ParamTest param)
     {
--- Assets/Script/TestEvent/TestObject.cs
+++ Assets/Script/TestEvent/TestObject.cs
@@ -1,16 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using STAROCK.Event;
 using UnityEngine;
 
 public class TestObject : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+
+    private void OnEnable()
     {
+        Debug.Log("Send events");
         EventManager.Send(EventIDs.Test1, new ParamTest(){ID=88});
         EventManager.Send(EventIDs.Test2, 666);
     }
-
-     
 }
